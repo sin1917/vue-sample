7e2a67d72eec5311342494ef1ebccf883c2fb5d1
sin1917 <103310011+sin1917@users.noreply.github.com>
2022-12-18 07:56:23 +0900

    vue test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ message }}</h1>
+    <input v-model="message">
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +36,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      message:this.msg,
+    }
   }
 }
 </script>
